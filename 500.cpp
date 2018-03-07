84bc314a4e239641ff92298569c94410b6053ffc
boubinjg <boubinjg@gmail.com>
2018-03-07 18:13:01 -0500

    500

--- 500.cpp
+++ 500.cpp
@@ -1,53 +1,34 @@
-#include <iostream>
-#include <math.h>
-int primes [500500];
-int init [500500];
-void getPrime(int num)
-{	
-	int count = 0;
-	for(int i = 2; count<num; i++)
-		for(int j = 2; j<=i; j++)
-		{
-			if(i == 2)
-			{
-				primes[count] = i;
-				count++;
-			}
-			if(i%j==0 && i!= 2)
-				break;
-			else if(i == j+1)	
-			{	
-				primes[count] = i;
-				count++;
-				std::cout<<i<<std::endl;
-			}
-		}
-}
-void initialize()
-{
-	for(int i = 0; i<500500; i++)
-		init[i] = 0;
-}
-void divisors()
-{
-	int zeroIndex = 0;
-	int changeIndex = 0;
-	int currentIndex = 0;
-	
-}
-void addUp()
-{	
-	for(int i = 0; i<20; i++)
-	{
-		std::cout<<primes[i]<<"^"<<init[i]<<std::endl;
+#include<iostream>
+#include<vector>
+#include<cmath>
+bool isPrime(int num) {	
+	for(int i = 2; i<=sqrt(num); i++) {
+	    if(!(num % i))
+	        return false;
 	}
+	return true;
 }
-int main()
-{
-	//get prime must provide 500500 for the actual solution
-	getPrime(1000);
-	initialize();
-	divisors();
-	addUp();
+int main() {
+	std::vector<int> divisorBase = {2};
+	long long sq = 0, number = 2;
+	long long mod = 500500507;
+	for(int i = 2; i<=500500; i++){
+        long long last = divisorBase[divisorBase.size()-1];
+        long long curPrime = last + 1;
+        long long lsquare = divisorBase[sq]*divisorBase[sq];
+        while(!isPrime(curPrime))
+            ++curPrime;
+        if(curPrime < lsquare) {
+            number *= curPrime;
+            number %= mod;
+            divisorBase.push_back(curPrime);
+        } else {
+            number *= lsquare;
+            number %= mod;
+            divisorBase.push_back(lsquare);
+            ++sq;
+        }
+	}
+    std::cout<<"The smallest number with 2^500500 divisors is: "<<number<<" In modulo 500500507"<<std::endl;
 	return 0;
 }
